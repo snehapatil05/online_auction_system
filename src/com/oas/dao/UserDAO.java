6fa0b7871ae3ca9286c3a92e88acebeb3c574ba3
lina-shejwal <54372171+lina-shejwal@users.noreply.github.com>
2019-08-25 21:16:33 -0700

    Update UserDAO.java
    changed return value of selectAllUsers() from User to List<User>

--- src/com/oas/dao/UserDAO.java
+++ src/com/oas/dao/UserDAO.java
@@ -86,15 +86,16 @@ public class UserDAO {
 		}
 	}
 	
-	public User selectAllUsers() throws DataAccessException {
+	public List<User> selectAllUsers() throws DataAccessException {
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
-		User user= new User();
+		List<User> users = new ArrayList<>();
+
 		try {
 			preparedStatement = this.connection.prepareStatement("SELECT * FROM USER_MASTER");
 			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
-			
+				User user= new User();
 				user.setUserID(resultSet.getInt(1));
 				user.setName(resultSet.getString(2));
 				user.setDob(resultSet.getDate(3));
@@ -106,15 +107,16 @@ public class UserDAO {
 				user.setUserType(resultSet.getString(9));
 				user.setWalletAmount(resultSet.getDouble(10));
 				
+				users.add(user);
+
 			}
-			return user;
+			return users;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DataAccessException("could not access records from USER_MASTER table");
 		}
 
 	}
-
 	public User selectUserByID(int id) throws DataAccessException {
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
@@ -174,14 +176,6 @@ public class UserDAO {
 			resultSet = preparedStatement.executeQuery();
 			while (resultSet.next()) {
 				passwordFromDB= resultSet.getString(1);
-			}
-			MessageDigest sha = MessageDigest.getInstance("SHA-1");
-			byte[] hashedBytes = sha.digest(password.getBytes());
-			char [] digits = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-			for(int idx = 0; idx < hashedBytes.length; ++idx) {
-				byte b= hashedBytes[idx];
-				hash.append(digits[(b & 0xf0) >> 4]);
-				hash.append(digits[b & 0xf0]);
 			}
 			if(passwordFromDB.equals(password)) {
 				return true;
